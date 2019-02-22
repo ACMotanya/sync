49ec17166477b09ad98ab097b7a3f661886ce9a9
Azubuike Motanya <acmotanya@gmail.com>
2019-02-22 16:29:09 -0500

    I think this is correct but I am not sure

--- app.js
+++ app.js
@@ -25,14 +25,56 @@ app.listen(3000, () => {
   console.log('The application is running the product sync.');
 });
 
-require('./routes/routes.js')(app);
+//require('./routes/routes.js')(app);
+
+app.get('/login', function(req, res){
+  res.render('login', {
+      title: 'Express Login'
+  });
+});
+
+app.get('/', (req, res) => {
+res.render('index-2');
+});
+
+app.get('/about', (req, res) => {
+res.render('card', {prompt: "Guys"});
+});
+
+app.get('/hello', (req, res) => {
+res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+res.render('hello', { name: req.body.username });
+});
+
+app.get('/save', function (req, res) {
+fs.writeFile('log.txt', 'This is my text', function (err) {
+  if (err) throw err;
+  console.log('Replaced!');
+  res.send('Replaced!');
+});
+});
+
+app.get('/getprods', function (req, res) {
+getProducts();
+});
+
+app.get('/getljevents', function (req, res) {
+getljevents();
+});
+
+app.get('/swdb800info', function (req, res) {
+getProducts800() ;
+});
 
 
 function getProducts() {
   sql.connect(dbconfig).then(pool =>  {
     //const request = new sql.Request();
     return pool.request()
-    .query("SELECT dbo.CCA_ITEM_DESCRIPTIONS.vItemNumber AS vItemNumber, dbo.CCA_ITEM_DESCRIPTIONS.vLocation AS vLocation, dbo.CCA_ITEM_DESCRIPTIONS.vDescription AS vDescription, dbo.CCA_ITEM_DESCRIPTIONS.vShortDesc AS vShortDesc,	dbo.CCA_ITEM_DESCRIPTIONS.vLook AS vLook, dbo.CCA_ITEM_DESCRIPTIONS.vSpecificColor AS vSpecificColor, dbo.CCA_ITEM_DESCRIPTIONS.vGenColor AS vGenColor, dbo.CCA_ITEM_DESCRIPTIONS.vGenMaterial AS vGenMaterial, dbo.CCA_ITEM_DESCRIPTIONS.vGenItemType AS vGenItemType, dbo.CCA_ITEM_DESCRIPTIONS.vShape AS vShape, dbo.CCA_ITEM_DESCRIPTIONS.vSizeType AS vSizetype, dbo.CCA_ITEM_DESCRIPTIONS.vMetalColor AS vMetalColor, dbo.CCA_ITEM_DESCRIPTIONS.vMetalType AS vMetalType, dbo.CCA_ITEM_DESCRIPTIONS.vDimensions AS vDimensions, dbo.CCA_ITEM_DESCRIPTIONS.vPcCounts AS vPcCounts, dbo.CCA_ITEM_DESCRIPTIONS.vKeywords AS vKeywords, dbo.CCA_ITEM_DESCRIPTIONS.vOnSale AS vOnSale, dbo.CCA_ITEM_DESCRIPTIONS.vFeaturedItem AS vFeaturedItem,  dbo.CCA_ITEM_DESCRIPTIONS.vSorting AS vSorting, dbo.CCA_ITEM_DESCRIPTIONS.vAggregation AS vAggregation, dbo.SWCCSSTOK.itemprice_1 AS itemprice_1, dbo.SWCCSSTOK.itemprice_2 AS itemprice_2, dbo.SWCCSSTOK.quantityonhand AS quantityonhand FROM dbo.CCA_ITEM_DESCRIPTIONS LEFT JOIN dbo.SWCCSSTOK ON dbo.CCA_ITEM_DESCRIPTIONS.vItemNumber = dbo.SWCCSSTOK.stocknumber AND dbo.CCA_ITEM_DESCRIPTIONS.vLocation = dbo.SWCCSSTOK.locationnumber WHERE dbo.CCA_ITEM_DESCRIPTIONS.vShowOnSite = 'Y' AND dbo.CCA_ITEM_DESCRIPTIONS.vLocation = '900'");
+    .query("SELECT dbo.CCA_ITEM_DESCRIPTIONS.vItemNumber AS vItemNumber, dbo.CCA_ITEM_DESCRIPTIONS.vLocation AS vLocation, dbo.CCA_ITEM_DESCRIPTIONS.vDescription AS vDescription, dbo.CCA_ITEM_DESCRIPTIONS.vShortDesc AS vShortDesc,	dbo.CCA_ITEM_DESCRIPTIONS.vLook AS vLook, dbo.CCA_ITEM_DESCRIPTIONS.vSpecificColor AS vSpecificColor, dbo.CCA_ITEM_DESCRIPTIONS.vGenColor AS vGenColor, dbo.CCA_ITEM_DESCRIPTIONS.vGenMaterial AS vGenMaterial, dbo.CCA_ITEM_DESCRIPTIONS.vGenItemType AS vGenItemType, dbo.CCA_ITEM_DESCRIPTIONS.vShape AS vShape, dbo.CCA_ITEM_DESCRIPTIONS.vSizeType AS vSizetype, dbo.CCA_ITEM_DESCRIPTIONS.vMetalColor AS vMetalColor, dbo.CCA_ITEM_DESCRIPTIONS.vMetalType AS vMetalType, dbo.CCA_ITEM_DESCRIPTIONS.vDimensions AS vDimensions, dbo.CCA_ITEM_DESCRIPTIONS.vPcCounts AS vPcCounts, dbo.CCA_ITEM_DESCRIPTIONS.vKeywords AS vKeywords, dbo.CCA_ITEM_DESCRIPTIONS.vOnSale AS vOnSale, dbo.CCA_ITEM_DESCRIPTIONS.vFeaturedItem AS vFeaturedItem,  dbo.CCA_ITEM_DESCRIPTIONS.vSorting AS vSorting, dbo.CCA_ITEM_DESCRIPTIONS.vAggregation AS vAggregation, dbo.SWCCSSTOK.itemprice_1 AS itemprice_1, dbo.SWCCSSTOK.itemprice_2 AS itemprice_2, dbo.SWCCSSTOK.quantityonhand AS quantityonhand FROM dbo.CCA_ITEM_DESCRIPTIONS LEFT JOIN dbo.SWCCSSTOK ON dbo.CCA_ITEM_DESCRIPTIONS.vItemNumber = dbo.SWCCSSTOK.stocknumber AND dbo.CCA_ITEM_DESCRIPTIONS.vLocation = dbo.SWCCSSTOK.locationnumber WHERE dbo.CCA_ITEM_DESCRIPTIONS.vLocation = '900'");
   }).then(result => {
       items = JSON.stringify(result.recordset);
 			items = JSON.parse(items.replace(/"\s+|\s+"/g,'"'));
@@ -58,6 +100,15 @@ function getProducts() {
         console.log("JSON has been created.");
       });
   }).then(() => {
+    var Client = require('ftp');
+    var c = new Client();
+    c.on('ready', function() {
+      c.put('items900.csv', 'items900-remote.csv', function(err) {
+        if (err) throw err;
+        c.end();
+        console.log("CSV has been created.");
+      });
+    });
     c.connect(cProps);
   }).then(() => {
     sql.close();
@@ -160,16 +211,7 @@ function getljevents() {
 
 
 
-var Client = require('ftp');
-var c = new Client();
-c.on('ready', function() {
-	c.put('items900.csv', 'items900-remote.csv', function(err) {
-		if (err) throw err;
-    c.end();
-    console.log("CSV has been created.");
-	});
-});
-//c.connect(cProps);
+
 
 
 function testing() {
@@ -219,9 +261,9 @@ function testing2() {
     });
 }
 
-testing2();
-//var cntr = 0;
-//var idx = 0;
+//testing2();
+var cntr = 0;
+var idx = 0;
 /*
 var itemdata = fs.readFileSync('oldevents.js', 'utf-8');
 itemdata = JSON.parse(itemdata);
@@ -318,12 +360,11 @@ function assignNewCats()
 {
   setTimeout( function() {
      data = {categories :  itemdata[idx].categories};
-    WooCommerce.put("products/" + itemdata[idx].id + "",data,  function(err, data, res){
+     WooCommerce.put("products/" + itemdata[idx].id + "", data,  function(err, data, res){
      
       result = JSON.parse(res);
     
       console.log(result.categories);
-      
      
       console.log(idx);
       idx++;
@@ -334,8 +375,164 @@ function assignNewCats()
     });
   }, 1000);
 }
-
-
 //assignNewCats();
 
 
+//////////////////////////////////////
+/////creating the simple products/////
+//////////////////////////////////////
+function updateSimple() {
+
+}
+
+
+
+/////////////////////////////////
+///// Cosmo Update Images ///////
+/////////////////////////////////
+function getImages() {
+  setTimeout( function() {
+    data = {
+      images: [
+        {
+          src: "https://cosmostylejewelry.com/CosmoImg/" + toAdd[idx] + ".jpg",
+          position: 0
+        },
+        {
+          src: "https://cosmostylejewelry.com/CosmoImg/" + toAdd[idx] + "-2.jpg",
+          position: 1
+        },
+        {
+          src: "https://cosmostylejewelry.com/CosmoImg/" + toAdd[idx] + "-3.jpg",
+          position: 2
+        }
+      ]
+    };
+   WooCommerce.put("products?filter[sku]=" + toAdd[idx] + "--400_PROD", data, function(err, data, res) {
+    
+     result = JSON.parse(res);
+     console.log(result);
+     console.log(idx);
+     idx++;
+     cntr++;
+     if (cntr <= toAdd.length) {
+       getImages();
+     }
+   });
+  }, 1000);
+}
+
+
+
+
+toAdd = ["C190550209",
+"C190550109",
+"C190552607",
+"C190560209",
+"C190560109",
+"C190562607",
+"C190570109",
+"C190572607",
+"C190570209",
+"C190580202",
+"C190580101",
+"C190582627",
+"C190590109",
+"C190590209",
+"C190592607",
+"C190630109",
+"C190630209",
+"C190632607",
+"C190650220",
+"C190650227",
+"C190670209",
+"C190670109",
+"C190680209",
+"C190680109",
+"C190682607",
+"C190690127",
+"C190690224",
+"C190760109",
+"C190760209",
+"C190762607",
+"C190790209",
+"C190792607",
+"C190790109",
+"C190800209",
+"C190800109",
+"C190810101",
+"C190822627",
+"C190820101",
+"C190860220",
+"C190860127",
+"C190870109",
+"C190870209",
+"C190872607",
+"C190880109",
+"C190880209",
+"C190882607",
+"C190890109",
+"C190890209",
+"C190892607",
+"C190930202",
+"C190940202",
+"C190940101",
+"C190950109",
+"C190950209",
+"C190952607",
+"C190960209",
+"C190962607",
+"C190960109",
+"C190980209",
+"C190980109",
+"C190982607",
+"C190990209",
+"C190990109",
+"C190992607",
+"C191000209",
+"C191000109",
+"C191002607",
+"C191010109",
+"C191010209",
+"C191012607",
+"C190840209",
+"C190840109",
+"C191280209",
+"C191280109",
+"C191282607",
+"C191300202",
+"C191300101",
+"C191302627",
+"C191400209",
+"C191400109",
+"C191402607",
+"C191390209",
+"C191390109",
+"C191392607",
+"C191420209",
+"C191420109",
+"C191820209",
+"C191820109",
+"C191822607",
+"C191840109",
+"C191840209",
+"C191842607",
+"C191850209",
+"C191850109",
+"19055PR",
+"19056PR",
+"19057PR",
+"19096PR",
+"19068PR",
+"19058PR",
+"19079PR",
+"19087PR",
+"19142NP",
+"19065BP",
+"19069NP",
+"19139NP",
+"C19394",
+"400GRG",
+"113RW"];
+
+//getImages();
